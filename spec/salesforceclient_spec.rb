d95d50c9744a6ee40a3ee44e368c21e744a85a1a
alessandrostein <alessandro@alessandrostein.com>
2014-07-30 14:14:04 -0300

    Create Lead and Test

--- spec/salesforceclient_spec.rb
+++ spec/salesforceclient_spec.rb
@@ -1,8 +1,19 @@
 require '../lib/rd_person/SalesforceClient'
+require '../lib/rd_person/Person'
 
 describe '#SalesforceClient' do
 
 	before do 
 		@new_salesforceclient = SalesforceClient.new
 	end
+
+	context 'Create a new Lead' do
+		it 'returns "ID"' do	
+			person = Person.new("Alessandro", "Stein", "alessandro@alessandrostein.com", "Resultados Digitais",
+			                    "Developer", "+55 47 0000 0000", "www.alessandrostein.com")
+
+			@new_salesforceclient.create(person)
+		end
+	end
+
 end
